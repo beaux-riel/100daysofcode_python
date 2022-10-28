202f17f4c2fd7c6b7146449c57ad88e39dd61032
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-10-28 07:39:14 -0700

    Day 2 - Much cleaner. Don't know what I was thinking.

--- day2.py
+++ day2.py
@@ -17,22 +17,19 @@
 print("Welcome to the tip calculator.")
 
 # Variable 1
-bill_total = input("What was the total bill? $")
-# Convert to float
-bill_total_float = float(bill_total)
+bill_total = float(input("What was the total bill? $"))
 
 # Variable 2
-desired_tip = input("What percentage tip would you like to give? ")
-# Convert to int + change input to 1.xx format for ease of calculating tip (12% = 1.12)
-desired_tip_integer = (float(desired_tip) * 0.01) + 1
+desired_tip = float(input("What percentage tip would you like to give? ")) 
 
 # Variable 3
-guest_count = input("How many people to split the bill? ")
-# Convert to int
-guest_count_integer = int(guest_count)
+guest_count = int(input("How many people to split the bill? "))
+
+# Final tip - conversion
+final_tip = (1 + desired_tip / 100)
 
 # Final bill calculation
-final_bill = ((bill_total_float * desired_tip_integer) / guest_count_integer)
+final_bill = ((bill_total * final_tip) / guest_count)
 
 # Print statement
 print(f'Each person should pay: ${final_bill:.2f}')
