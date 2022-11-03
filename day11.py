fa7c04c7a27844d66e68dccb29a86efdca17eac1
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-02 20:32:13 -0700

    Day 11 - The Blackjack Capstone Project

--- day11.py
+++ day11.py
@@ -37,7 +37,8 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 #user_cards = []
