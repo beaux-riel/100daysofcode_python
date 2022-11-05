ca9863b4b3e0e1c5d21adfb909896f6824f26bff
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-04 21:58:56 -0700

    Day 12 - unfinished

--- day12_game.py
+++ day12_game.py
@@ -1,5 +1,8 @@
 from random import randint
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 #Function to check user's guess against actual answer
 def check_answer(guess, answer):
     if guess > answer:
@@ -9,22 +12,25 @@ def check_answer(guess, answer):
     else:
         print("You got it! The answer was {answer}.")
 
+# Make function to set difficulty
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
 #Choose a random number between 1 ands 100
 print("Welcome to Beaux's Kick Ass Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 answer = randint(1, 100)
+print(f"Psst. The correct answer is: {answer}")
 
-# Make function to set difficulty
-def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
-
+turns = set_difficulty()
+print(f"You have {turns} attempts remaining to guess the number.")
 
 #Let the user guess a number
 guess = int(input("Make a guess: "))
-
-
-
-
 
 
 #Track the number of turns and reduce by 1 if they get it wrong
@@ -36,34 +42,34 @@ guess = int(input("Make a guess: "))
 
 
 
-game_in_progress = True
-target = (random.choice(range(100)))
+# game_in_progress = True
+# target = (random.choice(range(100)))
 
 
-print("I'm thinking of a number between 1 and 100.")
-print(f"That number is: {target}")
+# print("I'm thinking of a number between 1 and 100.")
+# print(f"That number is: {target}")
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+# difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-if difficulty == "hard":
-    guesses_remaining = 5
-    print(f"You have {guesses_remaining} guesses")
-else:
-    guesses_remaining = 10
-    print(f"You have {guesses_remaining} guesses")
+# if difficulty == "hard":
+#     guesses_remaining = 5
+#     print(f"You have {guesses_remaining} guesses")
+# else:
+#     guesses_remaining = 10
+#     print(f"You have {guesses_remaining} guesses")
 
-def guess():
-    global game_in_progress
+# def guess():
+#     global game_in_progress
 
 
     
 
-while game_in_progress:
-    guess()
-    guesses_remaining -= 1
+# while game_in_progress:
+#     guess()
+#     guesses_remaining -= 1
     
-    if guesses_remaining == 0:
-        game_in_progress = False
-    else:
-        print(f"You have {guesses_remaining} guesses left.")
+#     if guesses_remaining == 0:
+#         game_in_progress = False
+#     else:
+#         print(f"You have {guesses_remaining} guesses left.")
 
