a1d95a13b27bfcfcb6d8d68885f2389537322b98
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-09 20:46:52 -0800

    Day 18 - Turtle and the Graphical User Interface (GUI)

---
+++ day18/main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+import random
+
+beaux = Turtle()
+beaux.shape("turtle")
+beaux.color("DeepPink3")
+
+colors = ["blue", "purple", "lime", "sky blue", "pale green", "gold", "indigo", "red"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        beaux.forward(100)
+        beaux.right(angle)
+
+for shape_side_n in range(3, 11):
+    beaux.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+# def draw_line():
+#     for _ in range(15):
+#         beaux.forward(10)
+#         beaux.penup()
+#         beaux.forward(10)
+#         beaux.pendown()
+
+# draw_line()
+
+screen = Screen()
+screen.exitonclick()
