359256f0314bc80a4698d5ef34419b3b837d7fa0
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-13 22:51:23 -0800

    Day 21 - Build the Snake Game Part 2: Inheritance and List Slicing

--- day20/food.py
+++ day20/food.py
@@ -1,3 +1,7 @@
+# Day 21 of 100 Days of Code - Python
+# https://www.udemy.com/course/100-days-of-code/
+# Exercise - Build the Snake Game Part 2: Inheritance and List Slicing - Food Module
+
 from turtle import Turtle
 import random
 
