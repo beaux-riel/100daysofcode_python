ef0c1b4d58778918d00a1e242e160f7329d11815
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-17 23:04:18 -0800

    Day 23 + 24 - Files, Directories and Paths

--- day23/player.py
+++ day23/player.py
@@ -10,10 +10,19 @@ class Player(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("turtle")
-        self.penup()
         self.color("green")
-        self.goto(STARTING_POSITION)
+        self.penup()
+        self.go_to_start()
         self.setheading(90)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
