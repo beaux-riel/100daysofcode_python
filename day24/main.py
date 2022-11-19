27735055b39a01ae19c5a47c21f177bf3fe42333
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-18 23:21:03 -0800

    Day 24 - Files, Directories and Paths

---
+++ day24/main.py
@@ -0,0 +1,9 @@
+# Need to provide relative file path
+
+
+# with open('day24/beaux.txt') as file:
+#     contents = file.read()
+#     print(contents)
+
+with open('day24/new_file.txt', mode="w") as file:
+    file.write("\nNew text.")
