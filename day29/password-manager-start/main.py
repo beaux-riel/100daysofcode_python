8a38f088c9903c3dbdcc4bc4998377c11efe4043
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-11-23 21:49:03 -0800

    Day 29 - Building a Password Manager GUI App with Tkinter

--- day29/password-manager-start/main.py
+++ day29/password-manager-start/main.py
@@ -2,8 +2,20 @@
 # https://www.udemy.com/course/100-days-of-code/
 # Exercise - Building a Password Manager GUI App with Tkinter
 
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="day29/password-manager-start/logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+window.mainloop()
