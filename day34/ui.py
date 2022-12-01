b4391451c5908be552976bbd2d1cafd8891831a7
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-01 09:02:00 -0800

    Day 34 - UI as a class.

--- day34/ui.py
+++ day34/ui.py
@@ -1,2 +1,13 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Squiggly Quizzley")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.window.mainloop()
 
