aa92e575de3100615e50aa0f19e4a94718ae427a
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-01 23:12:58 -0800

    Day 34 - API based quiz completion

--- day34/ui.py
+++ day34/ui.py
@@ -39,9 +39,16 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
+        # Extracting this line from the if statement to ensure a white background
         self.canvas.config(bg="white")
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz, fool.")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_pressed(self):
         self.give_feedback(self.quiz.check_answer("True"))
