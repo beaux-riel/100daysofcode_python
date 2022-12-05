ec77e521763b7871712a0bfdddab65dacffcddef
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-05 15:06:07 -0800

    Day 37 - Habit Tracking Project: API Post Requests & Headers

--- day37/main.py
+++ day37/main.py
@@ -39,12 +39,26 @@ headers = {
 
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-today = datetime.now()
+today = datetime.datetime.now()
 
 pixel_data = {
-    "date": today.strftime("%Y%m%d"),
-    "quantity": "1.71",
+    "date": today.strftime('%Y%m%d'),
+    "quantity": input("How many kilometers did you run today? "),
 }
 
 response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "1.7",
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
