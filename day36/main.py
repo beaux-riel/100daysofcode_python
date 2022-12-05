8cd6507389aa1efe0859fdf6e8f8208e4875771b
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-04 22:59:09 -0800

    API requests

--- day36/main.py
+++ day36/main.py
@@ -1,10 +1,14 @@
 import requests
+from twilio.rest import Client
+import os
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+TWILIO_SID = os.environ.get("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 
 STOCK_API_KEY = "M9PA0M0A686DYSHP"
 NEWS_API_KEY = "b97d6c43f24441a68f44cfecd2f416f3"
@@ -32,7 +36,6 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
-
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
 print(difference)
@@ -51,7 +54,6 @@ if diff_percent < 10:
     news_response = requests.get(NEWS_ENDPOINT,params=news_params)
     news_response.json()["articles"]
 
-
 ## STEP 2: https://newsapi.org/ 
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
@@ -65,10 +67,17 @@ if diff_percent < 10:
 #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
 #TODO 9. - Send each article as a separate message via Twilio. 
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-
+for article in fromatted_articles:
+    message = client.messages.create(
+        body=article,
+        from_='+17207067009',
+        to='+16043639155'
+    )
 
 #Optional TODO: Format the message like this: 
 """
