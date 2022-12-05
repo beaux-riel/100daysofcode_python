b2654b2a3860821d591d6e1fb82bf6c7e34419c6
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-05 08:12:58 -0800

    Day 37 - Habit Tracking Project: API Post Requests & Headers

---
+++ day37/main.py
@@ -0,0 +1,43 @@
+import requests
+
+USERNAME = "b34ux"
+TOKEN = "supersecretpassword"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN, 
+    "username": USERNAME, 
+    "agreeTermsOfService": "yes", 
+    "notMinor": "yes"
+}
+
+# Comment out in order to continue exercise - User now created.
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Running Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai",
+
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+# Comment out in order to continue exercise - Graph now created.
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
