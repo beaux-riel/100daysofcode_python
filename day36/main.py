7e3f088f1e121c1823ea2436d78faabf47eae7ec
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-04 23:24:47 -0800

    Day 36 - Stock Trading News Alert Project

--- day36/main.py
+++ day36/main.py
@@ -7,8 +7,8 @@ COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-TWILIO_SID = os.environ.get("TWILIO_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_SID = "AC74c977b2f15e887c31e9fc9a6c11d43b"
+TWILIO_AUTH_TOKEN = "f78668ac58ac7fde6f71d30462659cce"
 
 STOCK_API_KEY = "M9PA0M0A686DYSHP"
 NEWS_API_KEY = "b97d6c43f24441a68f44cfecd2f416f3"
@@ -37,22 +37,26 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
 #TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
+difference = (float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+un_down = None
+if difference > 0:
+    up_down = "⬆"
+else:
+    up_down = "🔻"
 
 #TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round(difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent < 10:
+if abs(diff_percent) < 10:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
     }
 
     news_response = requests.get(NEWS_ENDPOINT,params=news_params)
-    news_response.json()["articles"]
+    articles = news_response.json()["articles"]
 
 ## STEP 2: https://newsapi.org/ 
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
@@ -67,12 +71,12 @@ if diff_percent < 10:
 #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\n Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
-for article in fromatted_articles:
+for article in formatted_articles:
     message = client.messages.create(
         body=article,
         from_='+17207067009',
