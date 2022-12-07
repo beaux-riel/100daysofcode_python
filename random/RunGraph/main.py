d56235408b02315c92ef0f1625386d76b39868e3
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-06 22:31:25 -0800

    Day 38 - Natural lanuage, workout tracking and APIs

--- random/RunGraph/main.py
+++ random/RunGraph/main.py
@@ -3,5 +3,6 @@
 from tkinter import *
 import webview
 
+# Shows graph but none of the rollover features/insights work
 window = webview.create_window('Beaux\'s Run Graph', 'https://pixe.la/v1/users/b34ux/graphs/graph1')
 webview.start()
