74563dfa9abc7c0bef4172a5cd7d7b08e5a4c27a
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-15 23:03:54 -0800

    Day 45 - Scraping with beautifulsoup

---
+++ day45/main.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open("day45/website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+all_anchor_tags = soup.find_all(name="a")
+
+# print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    print(tag.get("href"))
