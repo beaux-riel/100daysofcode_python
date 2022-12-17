92e4521ae6bfe0e8ea9d0b0bee08886568c60991
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-16 22:17:29 -0800

    Soup.find

--- day45/main.py
+++ day45/main.py
@@ -10,6 +10,11 @@ all_anchor_tags = soup.find_all(name="a")
 
 # print(all_anchor_tags)
 
-for tag in all_anchor_tags:
+# for tag in all_anchor_tags:
     # print(tag.getText())
-    print(tag.get("href"))
+    # print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = 
