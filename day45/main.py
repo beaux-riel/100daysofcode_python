1e2717767d4c198ff27a28db3f93d97ab25781ca
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-18 21:11:26 -0800

    BeautifulSoup exercises

--- day45/main.py
+++ day45/main.py
@@ -6,4 +6,10 @@ yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
 article_tag = soup.find(class_="titleline")
-print(article_tag)
+article_text = article_tag.getText()
+article_link = article_tag.get("href")
+article_upvote = soup.find(name="span", class_="score").getText()
+
+print(article_text)
+print(article_link)
+print(article_upvote)
