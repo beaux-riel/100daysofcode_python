ab8c53cd4b753febac0c3b323efeb22e074fd8f5
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-19 17:08:42 -0800

    BeautifulSoup scraping exercise

--- day45/main.py
+++ day45/main.py
@@ -5,11 +5,25 @@ response = requests.get("https://news.ycombinator.com")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-article_tag = soup.find(class_="titleline")
-article_text = article_tag.getText()
-article_link = article_tag.get("href")
-article_upvote = soup.find(name="span", class_="score").getText()
 
-print(article_text)
-print(article_link)
-print(article_upvote)
+articles = soup.find_all(class_="titleline")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.a.get("href")
+    article_links.append(link)
+    
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
+
+# Individual articles
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
