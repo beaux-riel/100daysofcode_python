b268aa2466ca3ce74198168935422eb2e474a755
beaux <34432201+beaux-riel@users.noreply.github.com>
2022-12-26 15:12:57 -0800

    Selenium and selectors

--- day48/main.py
+++ day48/main.py
@@ -4,13 +4,24 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = "day48/chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.amazon.ca/Windows-Mini-Single-Board-Computer/dp/B0BDFDTCD4/ref=sr_1_2?crid=1J27JDM0EUFRO&keywords=lattepanda&qid=1671687865&sprefix=latte+panda%2Caps%2C178&sr=8-2")
-price_symbol = driver.find_element(By.CLASS_NAME, "a-price-symbol")
-price_whole = driver.find_element(By.CLASS_NAME, "a-price-whole")
-price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
-price_total = f'{price_symbol.text}{price_whole.text}.{price_fraction.text}'
+driver.get("https://www.python.org")
+# price_symbol = driver.find_element(By.CLASS_NAME, "a-price-symbol")
+# price_whole = driver.find_element(By.CLASS_NAME, "a-price-whole")
+# price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+# price_total = f'{price_symbol.text}{price_whole.text}.{price_fraction.text}'
+# print(price_total)
 
-print(price_total)
+# search_bar = driver.find_element(By.NAME, "q")
+# print(search_bar)
+
+# logo = driver.find_element(By.CLASS_NAME, "python-logo")
+# print(logo.size)
+
+# documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
+# print(documentation_link.text)
+
+bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+print(bug_link.text)
 
 # driver.close()
 driver.quit()
