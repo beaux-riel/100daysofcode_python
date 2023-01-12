cff45d7fba31bddb639f9f340fb28e9b4d8ec032
beaux <34432201+beaux-riel@users.noreply.github.com>
2023-01-11 22:28:17 -0800

    More flask stuff

--- day54/main.py
+++ day54/main.py
@@ -4,15 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return "<h1 style='text-align: center'>Hello, World!</h1>" \
+            "<p>This is a paragraph.</p>" \
+            "<img src='https://i.chzbgr.com/full/7889288192/h1AD275FE/husky-puppies-on-the-porch-in-this-gif' width=200>"
 
 @app.route("/bye")
+# @make_bold
+# @make_emphasis
+# @make_underlined
 def goodbye_world():
     return "<h1>Goodbye, World!</h1>"
 
-@app.route("/username/<name>")
-def greet(name):
-    return f'Hello {name}!'
+@app.route("/username/<path:name>/<int:number>")
+def greet(name, number):
+    return f"Yo! What's going on {name}? Are you still {number} years old?"
 
 if __name__ == "__main__":
-    app.run()
+    #Run the app in debug mode to auto-reload
+    app.run(debug=True)
